5e629b8a8266d086cc8502a0b690857c0e7df47d
Mykaela Chávez <tatyana.chavez@ucsm.edu.pe>
2023-06-03 01:22:27 -0500

    fit: importando el csv en data

--- divorciados.py
+++ divorciados.py
@@ -8,3 +8,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
+
+# importando la data
+data = pd.read_csv("content/divorcios.csv", header=0, sep=";")
+print(data.head(10))
+print(data.shape)
+# porcentaje de personas divorciados
+print(np.average(data.Divorcio) * 100)
