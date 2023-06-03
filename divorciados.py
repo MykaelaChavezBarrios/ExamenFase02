b538a8a96e43ba2d467980d5da4406770bb73a48
Mykaela Chávez <tatyana.chavez@ucsm.edu.pe>
2023-06-03 01:20:05 -0500

    fit:import

---
+++ divorciados.py
@@ -0,0 +1,10 @@
+# AUTHOR: TATYANA MYKAELA CHAVEZ BARRIOS
+# tratamiento de datos
+import pandas as pd
+import numpy as np
+
+# entrenamiento
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
